49d6b0db5e74fa3b2b69401fdec81fb59b78b30b
aleli-b <alexredd95@gmail.com>
2025-09-27 03:52:47 -0300

    repo init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import First from "./sections/First";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <First />
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/First/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./index.css";
+
+
+const First = () => {
+    return (
+        <section className='first'>
+            <h1>First</h1>
+        </section>
+    );
+};
+
+export default First;
